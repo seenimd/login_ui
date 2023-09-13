246c48b933fb77cfb2887699896c0d335357e9cf
Seenimohamed <144776468+seenimd@users.noreply.github.com>
2023-09-13 14:09:58 +0530

    fourth commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color(0xFF275BEA);
+const kPrimaryColor = Color(0xFFFFFFFF);
+const kSecondaryColor = Color(0xFF09F116);
